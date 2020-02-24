48aa64a9d3390d2b51c45cae6eab3b77f66411f2
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-02-24 16:01:43 +0530

    Update fff.groovy

--- vars/fff.groovy
+++ vars/fff.groovy
@@ -12,8 +12,8 @@
 def call()
 {
 sh """
-   echo $http_code | curl -w '%{http_code}' --request GET 'https://dev.azure.com/vickysastryvs/d2/_apis/git/pullrequests?searchCriteria.status=all&api-version=5.1' \
---header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206MnF4dmNoZWp2bzVzdHNzc3gyZXl5Mmp1azZ3ZHR6bnQ0eWJsb2Fwd2drd2FkdGQ0ZDNkYQ=='
+   curl -w '%{http_code}' --request GET 'https://dev.azure.com/vickysastryvs/d2/_apis/git/pullrequests?searchCriteria.status=all&api-version=5.1' \
+--header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206MnF4dmNoZWp2bzVzdHNzc3gyZXl5Mmp1azZ3ZHR6bnQ0eWJsb2Fwd2drd2FkdGQ0ZDNkYQ==' -o mani.txt
  """
- sh 'echo $http_code'
+ 
 }
