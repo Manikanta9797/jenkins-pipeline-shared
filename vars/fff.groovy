b113508a65599ec4485017b3e0b1d68899fecec7
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-02-24 15:48:12 +0530

    Update fff.groovy

--- vars/fff.groovy
+++ vars/fff.groovy
@@ -12,7 +12,7 @@
 def call()
 {
 sh """
-   curl --location --request GET 'https://dev.azure.com/vickysastryvs/d2/_apis/git/pullrequests?searchCriteria.status=all&api-version=5.1' \
+   curl -w '%{http_code}' --request GET 'https://dev.azure.com/vickysastryvs/d2/_apis/git/pullrequests?searchCriteria.status=all&api-version=5.1' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206MnF4dmNoZWp2bzVzdHNzc3gyZXl5Mmp1azZ3ZHR6bnQ0eWJsb2Fwd2drd2FkdGQ0ZDNkYQ=='
  """
  
