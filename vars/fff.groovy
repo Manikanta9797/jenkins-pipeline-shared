b995a95b69c90e784c88d97e3b158b4078084302
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-02-24 15:35:00 +0530

    Update fff.groovy

--- vars/fff.groovy
+++ vars/fff.groovy
@@ -1,19 +1,18 @@
 import groovy.json.JsonSlurper 
 
 @NonCPS
-deleteProject(){
-def jsonSlurper = new JsonSlurper() 
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/azd/out.json"),"UTF-8"))
-def resultJson = jsonSlurper.parse(reader)
-def projectid = resultJson.id
+pullreq(){
+//def jsonSlurper = new JsonSlurper() 
+//def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/azd/out.json"),"UTF-8"))
+//def resultJson = jsonSlurper.parse(reader)
+//def projectid = resultJson.id
  
  sh """
-   curl --location --request GET 'https://dev.azure.com/vickysastryvs/_apis/projects/mani/teams?api-version=5.1' \
---header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ==' -o team.json
- 
+   curl --location --request GET 'https://dev.azure.com/vickysastryvs/d2/_apis/git/pullrequests?searchCriteria.status=all&api-version=5.1' \
+--header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206MnF4dmNoZWp2bzVzdHNzc3gyZXl5Mmp1azZ3ZHR6bnQ0eWJsb2Fwd2drd2FkdGQ0ZDNkYQ=='
  """
 }
 def call()
 {
-deleteProject()
+pullreq()
 }
