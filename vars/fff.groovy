1b30cbda86a3a10843d56c4e91b22c70203d88fa
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-02-24 15:43:49 +0530

    Update fff.groovy

--- vars/fff.groovy
+++ vars/fff.groovy
@@ -1,18 +1,19 @@
-import groovy.json.JsonSlurper 
+//import groovy.json.JsonSlurper 
 
-@NonCPS
-pullreq(){
+//@NonCPS
+//pullreq(){
 //def jsonSlurper = new JsonSlurper() 
 //def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/azd/out.json"),"UTF-8"))
 //def resultJson = jsonSlurper.parse(reader)
 //def projectid = resultJson.id
+//} 
  
- sh """
+
+def call()
+{
+sh """
    curl --location --request GET 'https://dev.azure.com/vickysastryvs/d2/_apis/git/pullrequests?searchCriteria.status=all&api-version=5.1' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206MnF4dmNoZWp2bzVzdHNzc3gyZXl5Mmp1azZ3ZHR6bnQ0eWJsb2Fwd2drd2FkdGQ0ZDNkYQ=='
  """
-}
-def call()
-{
-pullreq()
+ 
 }
