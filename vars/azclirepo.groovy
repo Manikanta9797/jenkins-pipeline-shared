cf684dfaaa6dbfde427e829cba147fe24bf35d5d
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-02-26 15:16:43 +0530

    Create azclirepo.groovy

---
+++ vars/azclirepo.groovy
@@ -0,0 +1,15 @@
+/*def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.environments.environment)
+String a=jsonObj.environments.environment.deploy.key
+//String a=jsonObj.alm.projects.project.name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+env.name = projectName
+*/
+def call(){
+sh """    
+  az repos create --name rrr --organization https://dev.azure.com/vickysastryvs --project d2
+"""
+}
