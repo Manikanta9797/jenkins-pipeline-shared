a9f74a2f77d37eea706250c45f7ff1213a85983e
Manikanta Komaravelli <123manikanta45@gmail.com>
2020-02-26 18:14:32 +0530

    Update azclirepo.groovy

--- vars/azclirepo.groovy
+++ vars/azclirepo.groovy
@@ -9,8 +9,8 @@ String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
 env.name = projectName
 */
 def call(){
-  sh 'az extension add --name azure-devops' 
-  sh 'az devops login --organization https://dev.azure.com/vickysastryvs/'
+  //sh 'az extension add --name azure-devops' 
+  sh 'cat /home/ec2-user/token.txt | az devops login --organization https://dev.azure.com/vickysastryvs/'
 sh 'az repos create --name rrr --organization https://dev.azure.com/vickysastryvs --project d2'
 
 }
